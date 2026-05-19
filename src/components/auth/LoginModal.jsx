61f115ce52f80c7d78fb089b0c6ec49886edbbdd
8rene <irene.borja@dorsu.edu.ph>
2026-05-19 17:11:25 +0800

    Login modal

--- src/components/auth/LoginModal.jsx
+++ src/components/auth/LoginModal.jsx
@@ -176,12 +176,6 @@ const LoginModal = ({ onLogin, onClose, onSwitchToSignUp }) => {
             Continue with Google
           </button>
 
-          <button type="button" className="login-social-btn">
-            <svg width="18" height="18" viewBox="0 0 24 24" fill="#1877F2">
-              <path d="M24 12.073C24 5.405 18.627 0 12 0S0 5.405 0 12.073c0 6.027 4.388 11.025 10.125 11.927v-8.437H7.078v-3.49h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.49h-2.796v8.437C19.612 23.098 24 18.1 24 12.073z"/>
-            </svg>
-            Continue with Facebook
-          </button>
         </div>
 
         {/* Footer */}
